4e19e5f1db4b45a271ba9cf6059d2e15360d4e1a
vikramthakur <vikram.thakur788@gmail.com>
2021-04-22 12:07:17 +1000

    first commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useQuery} from 'react-query'
+import {Drawer, LinearProgress, Grid, Badge} from '@material-ui/core'
+import {AddShoppingCart} from '@material-ui/icons'
+
+export type CartItemType = {
+  id: number
+  category: string
+  description: string
+  image: string
+  price: number
+  title: string 
+  amount: number
+
+}
+const getProducts = async (): Promise<CartItemType[]> => 
+await (await fetch("https://fakestoreapi.com/products")).json()
 
 function App() {
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts)
+  console.log(data)
+  if(isLoading)
+    return <LinearProgress/>
+  if(error)
+    return <div> Oops! Something went wrong....</div>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Start
+     
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {QueryClient, QueryClientProvider} from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.render(
-  <React.StrictMode>
+  <QueryClientProvider client = {queryClient}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </QueryClientProvider>
+,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
